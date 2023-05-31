1c8f59257358e40e79007778a4b29fa45fb5b5aa
Louis Gheysens <Louis.Gheysens@norriq.com>
2023-05-31 13:21:35 +0200

    Bugfix foreign key

--- BookingData/Migrations/20230531112005_ReplaceForeignKey.Designer.cs
+++ BookingData/Migrations/20230531112005_ReplaceForeignKey.Designer.cs
@@ -4,6 +4,7 @@ using BookingData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingData.Migrations
 {
     [DbContext(typeof(BookingDbContext))]
-    partial class BookingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230531112005_ReplaceForeignKey")]
+    partial class ReplaceForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookingData/Migrations/20230531112005_ReplaceForeignKey.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingData.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Taxi_Drivers_DriverId1",
+                table: "Taxi");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Taxi_DriverId1",
+                table: "Taxi");
+
+            migrationBuilder.DropColumn(
+                name: "DriverId1",
+                table: "Taxi");
+
+            migrationBuilder.DropColumn(
+                name: "TaxiId",
+                table: "Drivers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Taxi_DriverId",
+                table: "Taxi",
+                column: "DriverId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Taxi_Drivers_DriverId",
+                table: "Taxi",
+                column: "DriverId",
+                principalTable: "Drivers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Taxi_Drivers_DriverId",
+                table: "Taxi");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Taxi_DriverId",
+                table: "Taxi");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DriverId1",
+                table: "Taxi",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TaxiId",
+                table: "Drivers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Taxi_DriverId1",
+                table: "Taxi",
+                column: "DriverId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Taxi_Drivers_DriverId1",
+                table: "Taxi",
+                column: "DriverId1",
+                principalTable: "Drivers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
